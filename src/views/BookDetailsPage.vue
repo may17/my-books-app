56c3d8b07831437b1330f9ae9e66e6188c665b2e
Joe Ray Gregory <joe@may17.de>
2021-11-08 10:38:43 +0100

    Add solution

--- src/views/BookDetailsPage.vue
+++ src/views/BookDetailsPage.vue
@@ -1,12 +1,33 @@
 <template>
-  <h1>Details</h1>
-  <p>{{ $route.params.id }}</p>
+  <h1>{{ book.title }}</h1>
+  <h2>{{ book.subtitle }}</h2>
+  <p>{{ book }}</p>
 </template>
 
 <script>
 export default {
-  created() {
-    console.log(this);
+  data() {
+    return {
+      book: [],
+    };
+  },
+  async created() {
+    const response = await fetch(
+      "http://localhost:3000/books/" + this.$route.params.id
+    );
+    const jsonData = await response.json();
+
+    this.book = jsonData;
   },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 1.5rem;
+}
+
+h2 {
+  font-size: 1.2rem;
+}
+</style>
